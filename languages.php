9d54255347db39f45dbac817c2cf964d5bb6054f
Hatem Mohamed Elsheref <hatem_mohamed_elsheref@yahoo.com>
2020-10-03 15:32:16 +0000

    languages.php created online with Bitbucket

---
+++ languages.php
@@ -0,0 +1,25 @@
+<?php
+return [
+
+    'default'           =>'en',
+    'lang'              =>[
+        'en'=>[
+            'name'          =>'English',
+            'translation'   =>'English',
+            'direction'     =>'ltr',
+            'status'        =>true
+        ],
+        'ar'=>[
+            'name'          =>'Arabic',
+            'translation'   =>'العربية',
+            'direction'     =>'rtl',
+            'status'        =>true
+        ],
+        'fr'=>[
+            'name'          =>'French',
+            'translation'   =>'French',
+            'direction'     =>'ltr',
+            'status'        =>true
+        ]
+    ]
+];
